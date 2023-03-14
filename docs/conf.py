fe44639c0c8d43fa9ab9ea7ab88809f0452f443c
Martin Soria Røvang <martinrovang@gmail.com>
2023-03-14 19:37:20 +0000

    Better time of update to docs

--- docs/conf.py
+++ docs/conf.py
@@ -8,32 +8,36 @@ import os
 import sys
 import toml
 
-sys.path.insert(0, os.path.abspath('..'))
+sys.path.insert(0, os.path.abspath(".."))
 
 # get version from pyproject.toml
-with open('../pyproject.toml') as f:
+with open("../pyproject.toml") as f:
     pyproject = toml.load(f)
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Gingerbread'
-copyright = '2023, Martin Soria Røvang'
-author = 'Martin Soria Røvang'
-release = pyproject['tool']['poetry']['version']
+project = "Gingerbread"
+copyright = "2023, Martin Soria Røvang"
+author = "Martin Soria Røvang"
+release = pyproject["tool"]["poetry"]["version"]
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-extensions = ['sphinx.ext.autodoc', 
-              'sphinx.ext.napoleon',
-              'sphinx.ext.coverage',]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.coverage",
+]
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-master_doc = 'index'
+master_doc = "index"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'furo'
+html_theme = "furo"
 html_logo = "63ecff6168e0e.png"
+
+today_fmt = "%b %d %y at %H:%M"
