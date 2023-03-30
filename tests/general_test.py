531ec94178c22d3a1d95d5051c518c65011df488
Martin Soria Røvang <martinrovang@gmail.com>
2023-03-30 10:14:18 +0200

    removed data_loader and lightning, added loguru for cleaner logger setup, added beartype for type checking in test

--- tests/general_test.py
+++ tests/general_test.py
@@ -6,15 +6,14 @@ sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/..")
 import central_processing
 import data_download
 import cli_main
-import data_loader.data_handling
 
 
 class Test_General:
+
     def test_imports(self):
         assert central_processing
         assert data_download
         assert cli_main
-        assert data_loader.data_handling
 
     def test_central_processing(self):
         central_processing.CentralProcessing()
