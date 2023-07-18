aeb0c7c16d44c278108ba27fa3b97c8967f1848d
Martin Soria Røvang <martinrovang@gmail.com>
2023-07-18 12:05:29 +0200

    cleanup

--- config/config.py
+++ config/config.py
@@ -12,30 +12,6 @@ ROOT: os.PathLike = os.getcwd()
 
 REQUIRED_SHAPE = 3
 
-# ---------------------------------------------------------
-# pymedquery credentials
-MQUSER = os.environ.get("MQUSER")
-MQPWD = os.environ.get("MQPWD")
-DATABASE = os.environ.get("DATABASE")
-PGSSLKEY = os.environ.get("PGSSLKEY")
-PGSSLCERT = os.environ.get("PGSSLCERT")
-PGSSLROOTCERT = os.environ.get("PGSSLROOTCERT")
-MQHOST = os.environ.get("MQHOST")
-MQPORT = os.environ.get("MQPORT")
-
-# ---------------------------------------------------------
-ENVPATH: os.PathLike = os.environ.get("PATH")
-CRON_ENV: Dict[str, str] = {
-    "PATH": ENVPATH,
-    "PYTHONPATH": ROOT,
-    "MQUSER": MQUSER,
-    "MQPWD": MQPWD,
-    "DATABASE": DATABASE,
-    "PGSSLKEY": PGSSLKEY,
-    "PGSSLROOTCERT": PGSSLROOTCERT,
-    "PGSSLCERT": PGSSLCERT,
-}
-
 # EXTRAS
 DATA_SAVE_DIR = "./data"
 
@@ -44,7 +20,6 @@ PROJECTMETADATAURL = "https://raw.githubusercontent.com/NeoMedSys/gingerbread_sc
 
 
 class BaseModel(PydanticBaseModel):
-
     class Config:
         arbitrary_types_allowed = True
 
--- xmodules/optimizers/optim_adam.py
+++
@@ -1,6 +0,0 @@
-import torch
-
-class LitAdam(torch.optim.Adam):
-    def step(self, closure):
-        print("⚡", "using LitAdam", "⚡")
-        super().step(closure)
